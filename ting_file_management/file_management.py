1f903b63e2fff5b17ef12e7378896e63f01ef7f5
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-06-06 14:56:19 -0300

    refactor: with open file

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -3,16 +3,14 @@ import sys
 
 def txt_importer(path_file):
     try:
-        file = open(path_file, "r")
         file_extension = path_file.split(".")[-1]
 
         if file_extension != "txt":
             sys.stderr.write("Formato inválido ")
 
-        content = file.read()
-        content_list = content.split("\n")
-        file.close()
+        with open(path_file) as file:
+            content = file.read()
+            return content.split("\n")
 
-        return content_list
     except FileNotFoundError:
         sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
