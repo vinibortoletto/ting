2ec1eb71f0e2c754aa08f10e57bebb163e49af15
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-06-06 14:53:27 -0300

    feat: read txt file

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,18 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+        file = open(path_file, "r")
+        file_extension = path_file.split(".")[-1]
+
+        if file_extension != "txt":
+            sys.stderr.write("Formato inválido ")
+
+        content = file.read()
+        content_list = content.split("\n")
+        file.close()
+
+        return content_list
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
