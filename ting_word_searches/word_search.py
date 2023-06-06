e0134cedae4c8c06bef31f77eebc5e4c05f0deab
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-06-06 17:33:12 -0300

    feat: word search

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,26 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    word_occurrence_list = []
+
+    for index in range(len(instance)):
+        content = instance.search(index)
+        line_list = content["linhas_do_arquivo"]
+
+        for sub_index in range(len(line_list)):
+            line = line_list[sub_index].lower()
+
+            if word in line:
+                word_occurrence_list.append({"linha": sub_index + 1})
+
+    if len(word_occurrence_list) == 0:
+        return []
+
+    return [
+        {
+            "palavra": word,
+            "arquivo": content["nome_do_arquivo"],
+            "ocorrencias": word_occurrence_list,
+        }
+    ]
 
 
 def search_by_word(word, instance):
