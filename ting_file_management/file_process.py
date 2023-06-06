4fadb401c160ee4eb6461db6f667d23dd33af05a
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-06-06 15:11:31 -0300

    feat: file process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,18 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    content_list = txt_importer(path_file)
+
+    processed_content = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(content_list),
+        "linhas_do_arquivo": content_list,
+    }
+
+    instance.enqueue(processed_content)
+    sys.stdout.write(str(processed_content))
 
 
 def remove(instance):
