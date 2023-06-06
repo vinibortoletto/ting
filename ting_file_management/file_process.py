d41508d4597308799226341e0175623a4b48e6aa
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-06-06 15:52:42 -0300

    feat: get file metadata

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -31,4 +31,8 @@ def remove(instance):
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        file = instance.search(position)
+        sys.stdout.write(str(file))
+    except IndexError:
+        sys.stderr.write("Posição inválida")
